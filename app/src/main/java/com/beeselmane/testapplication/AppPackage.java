10ac3c1a3faa90053ed7a54d106a6430716b9ec9
ConnorB123 <connor.j.byrnes@gmail.com>
2016-01-02 17:06:24 -0500

    stuff

---
+++ app/src/main/java/com/beeselmane/testapplication/AppPackage.java
@@ -0,0 +1,11 @@
+package com.beeselmane.testapplication;
+
+import android.graphics.drawable.Drawable;
+
+public class AppPackage
+{
+    public CharSequence label;
+    public String publicName;
+    public CharSequence name;
+    public Drawable icon;
+}
